e9c818a25a16269b3f7db8da411c48059808eb84
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-22 10:18:50 +0200

    Testing variables

---
+++ declare-variable/src/TestingDouble.java
@@ -0,0 +1,14 @@
+
+public class TestingDouble {
+	
+	public static void main(String[] args) {
+		
+		double salary = 1250.70;
+		System.out.println("The salary is "+salary);
+		
+		double division = 3.14 / 2;
+		System.out.println("The result is "+division);
+		
+	}
+
+}
---
+++ declare-variable/src/TestingVariables.java
@@ -0,0 +1,19 @@
+
+public class TestingVariables {
+	
+	public static void main(String[] args ) {
+		
+		int age = 37;
+		System.out.println(age);
+		
+		age = 30 + 10;
+		System.out.println(age);
+		
+		age = (7*5)+2;
+		System.out.println(age);
+		
+		System.out.println("The age is: "+age);
+	
+	}
+
+}
