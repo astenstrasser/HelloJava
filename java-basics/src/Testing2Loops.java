870af9b51656c1882fa97f7dd7c266326e4b82e6
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-23 11:46:00 +0200

    Testing 2 loops

---
+++ java-basics/src/Testing2Loops.java
@@ -0,0 +1,15 @@
+
+public class Testing2Loops {
+
+	public static void main(String[] args) {
+		for (int multiplier = 1; multiplier <= 10; multiplier++) {
+			for (int tracker = 0; tracker <= 10; tracker++) {
+				System.out.print(multiplier * tracker);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
