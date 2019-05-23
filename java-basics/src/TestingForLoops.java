b296ec10f83cf3a7daa45ac29930afc758c8d53d
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-23 11:29:55 +0200

    Syntax - For loops

---
+++ java-basics/src/TestingForLoops.java
@@ -0,0 +1,16 @@
+
+public class TestingForLoops {
+
+	public static void main(String[] args) {
+
+// 	for syntax: for (declaring variable, executed only once ; 
+//		              boolean condition to know if the loop should be executed ; 
+//		              what should be executed at the end of every repetition)
+
+		for (int tracker = 0; tracker <= 10; tracker++) {
+			System.out.println(tracker);
+		}
+
+//	it is not possible to print the variable tracker outside the for statement.
+	}
+}
