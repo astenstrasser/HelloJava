6e4466fe92fad79968f017bd0349bf7fca188165
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-23 11:59:27 +0200

    Loops like matrix, altering condition on for statement

---
+++ java-basics/src/Testing2Loops2.java
@@ -0,0 +1,15 @@
+
+public class Testing2Loops2 {
+
+	public static void main(String[] args) {
+		for (int row = 0; row <= 10; row++) {
+			for (int column = 0; column <= row; column++) {
+				System.out.print("*");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
