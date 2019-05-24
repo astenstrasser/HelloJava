ec64b2eec2038a892b43e74a0e586ce2254c5fdd
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-24 21:33:16 +0200

    Added transfer method

--- neo-bank/src/Account.java
+++ neo-bank/src/Account.java
@@ -3,25 +3,30 @@ public class Account {
 	int agency;
 	int accountNumber;
 	String accountHolder;
-	
+
 	// to set a start value for a property, just do it at class definition:
-	// int agency = 01; 
-	
+	// int agency = 01;
+
 	public void deposit(double value) {
 		this.balance += value;
 	}
-	
+
 //	void is used here to inform that this method won't return anything.
 
 	public boolean withdrawal(double value) {
 		if (this.balance >= value) {
 			this.balance -= value;
 			return true;
-		} else {
-			return false;
 		}
+		return false;
 	}
-	
+
+	public boolean transfer(double value, Account destinationAccount) {
+		if (this.withdrawal(value)){
+			destinationAccount.deposit(value);
+			return true;
+		}
+		return false;
+	}
+
 }
-
-
