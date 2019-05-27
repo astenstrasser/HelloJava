d6852947e7e8ed6787e5bb5b633190905207082f
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-27 21:44:47 +0200

    Inheritance of overwrited methods with super

--- neo-bank4-heritage/src/Employee.java
+++ neo-bank4-heritage/src/Employee.java
@@ -3,10 +3,13 @@ public class Employee {
 
 	private String name;
 	private String taxNumber;
-	protected double salary;
+//	protected double salary;
 	
-//	the protected modifier implicates that this attribute 
+//	The protected modifier implicates that this attribute 
 //	is valid for this class and for children classes. 
+//	But we still want to keep it private. Is always better to use it encapsulated.
+//	We can use the getter getSalary() to access it from other files.  
+	private double salary;
 
 	public double getBonus() {
 		return this.salary * 0.1;
--- neo-bank4-heritage/src/Manager.java
+++ neo-bank4-heritage/src/Manager.java
@@ -5,10 +5,13 @@ public class Manager extends Employee {
 	
 	private int password;
 	
-	public double getBonus() {
-		return super.salary;
-	}
 	
+//	The rule is: Managers get the usual bonus that every employee receives, added to their salary
+	public double getBonus() {
+		return super.getBonus() + super.getSalary();
+	}
+//	calling the method using super is possible to call the method with the same name in mother class.
+//	If we would use salary as protected attribute, we could use super.salary to refer to it.
 //	this.salary would make developers expect this attribute salary to be in class Manager.
 //	super.salary makes clear that this attribute is inherited from mother class.
 	
