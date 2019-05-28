19230ea4781d6a311230d6d127104bda574106e2
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-28 14:47:32 +0200

    setting classes

--- neo-bank5/src/Account.java
+++ neo-bank5/src/Account.java
@@ -1,11 +1,19 @@
 
-public class BankAccount {
-
+public class Account {
 
 	private double balance;
 	private int agency;
 	private int accountNumber;
 	private Client accountHolder;
+	private static int total;
+
+	public Account(int agency, int accountNumber, Client accountHolder) {
+		Account.total++;
+		this.agency = agency;
+		this.accountNumber = accountNumber;
+		this.accountHolder = accountHolder;
+		this.balance = 0;
+	}
 
 	public void deposit(double value) {
 		this.balance += value;
@@ -19,7 +27,7 @@ public class BankAccount {
 		return false;
 	}
 
-	public boolean transfer(double value, BankAccount destinationAccount) {
+	public boolean transfer(double value, Account destinationAccount) {
 		if (this.withdrawal(value)) {
 			destinationAccount.deposit(value);
 			return true;
@@ -36,6 +44,10 @@ public class BankAccount {
 	}
 
 	public void setAgency(int agency) {
+		if (agency <= 0) {
+			System.out.println("Can not set a value <= 0 as agency");
+			return;
+		}
 		this.agency = agency;
 	}
 
@@ -44,15 +56,24 @@ public class BankAccount {
 	}
 
 	public void setAccountNumber(int accountNumber) {
+		if (accountNumber <= 0) {
+			System.out.println("Can not set a value <= 0 as account number");
+			return;
+		}
 		this.accountNumber = accountNumber;
 	}
 
 	public Client getAccountHolder() {
 		return this.accountHolder;
 	}
-
+	
 	public void setAccountHolder(Client accountHolder) {
 		this.accountHolder = accountHolder;
+	}
+	
+
+	public static int getTotal() {
+		return total;
 	}
 
 }
---
+++ neo-bank5/src/Client.java
@@ -0,0 +1,35 @@
+
+public class Client {
+
+	private String clientName;
+	private String taxNumber;
+	private String profession;
+
+
+	public Client(String clientName, String taxNumber, String profession) {
+		this.clientName = clientName;
+		this.taxNumber = taxNumber;
+		this.profession = profession;
+	}
+
+	public Client(String clientName, String taxNumber) {
+		this(clientName, taxNumber, "Student");
+	}
+
+	public String getClientName() {
+		return this.clientName;
+	}
+
+	public String getTaxNumber() {
+		return this.taxNumber;
+	}
+
+	public String getProfession() {
+		return this.profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+}
