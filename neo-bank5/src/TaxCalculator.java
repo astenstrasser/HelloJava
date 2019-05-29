a43dbd9b2f1edcffa33171d5b07146129e80bf00
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-29 18:54:10 +0200

    Class to calculate taxes from taxables

---
+++ neo-bank5/src/TaxCalculator.java
@@ -0,0 +1,15 @@
+
+public class TaxCalculator {
+	
+	private double totalTaxes;
+	
+	public void register(Taxable t) {
+//	everything that implements the interface taxable can be the input here
+		double value = t.getTaxes();
+		this.totalTaxes += value;
+	}
+	
+	public double getTotalTaxes() {
+		return totalTaxes;
+	}
+}
