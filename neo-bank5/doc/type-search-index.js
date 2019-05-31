3e12c2c17b15db3db125c89c6ea4abb176c37804
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-31 19:11:25 +0200

    Javadoc

---
+++ neo-bank5/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"br.com.neobank.bank.model","l":"Account"},{"l":"All Classes","url":"allclasses-index.html"}]
--- neo-bank5/src/br/com/neobank/bank/model/Account.java
+++ neo-bank5/src/br/com/neobank/bank/model/Account.java
@@ -1,13 +1,26 @@
 package br.com.neobank.bank.model;
 
-public abstract class Account {
+/** 
+ * Abstract Class that represents an generic account
+ * 
+ * @author Alexandra M. Stens.
+ * @version 1.0
+ */
 
+public abstract class Account {
+	
 	private double balance;
 	private int agency;
 	private int accountNumber;
 	private Client accountHolder;
 	private static int total;
 
+	/**
+	 * Constructor method to initialize account object from agency number, account number and a client as account holder.
+	 * @param agency
+	 * @param accountNumber
+	 * @param accountHolder
+	 */
 	public Account(int agency, int accountNumber, Client accountHolder) {
 		Account.total++;
 		this.agency = agency;
@@ -16,16 +29,31 @@ public abstract class Account {
 		this.balance = 0;
 	}
 	
+	/**
+	 * Constructor method to initialize account object from agency number and account number.
+	 * @param agency
+	 * @param accountNumber
+	 */
 	public Account(int agency, int accountNumber) {
 		Account.total++;
 		this.agency = agency;
 		this.accountNumber = accountNumber;
 	}
 
+	/**
+	 * Deposit given value in bank account.
+	 * @param value
+	 */
 	public void deposit(double value) {
 		this.balance += value;
 	}
 
+	/**
+	 * Method to withdrawal money from an account.
+	 * Balance should be greater then withdrawal value.
+	 * @param value
+	 * @throws NotEnoughMoneyException
+	 */
 	public void withdrawal(double value) throws NotEnoughMoneyException {
 		if (this.balance < value) {
 			throw new NotEnoughMoneyException("Not enough Money");
@@ -33,6 +61,12 @@ public abstract class Account {
 		this.balance -= value; 
 	}
 
+	/**
+	 * Transfer given value from account to destination account.
+	 * @param value
+	 * @param destinationAccount
+	 * @throws NotEnoughMoneyException
+	 */
 	public void transfer(double value, Account destinationAccount) throws NotEnoughMoneyException {
 		this.withdrawal(value);
 		destinationAccount.deposit(value);
