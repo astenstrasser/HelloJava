6ef61203815834fe88c66b174ca37602c34d6ca8
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-04 22:40:48 +0200

    Added test for 2 highest bids method

--- unit-tests/src/br/com/alura/auction/service/tests/TestAuctioneer.java
+++ unit-tests/src/br/com/alura/auction/service/tests/TestAuctioneer.java
@@ -1,6 +1,9 @@
 package br.com.alura.auction.service.tests;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 
 import br.com.alura.auction.domain.Auction;
@@ -26,12 +29,44 @@ public class TestAuctioneer {
 
 		Auctioneer auctioneer = new Auctioneer();
 		auctioneer.evaluate(auction);
-		System.out.println(auctioneer.getHighestBid());
-		System.out.println(auctioneer.getLowestBid());
 
-		Assert.assertEquals(400, auctioneer.getHighestBid(), 0.00001);
-		Assert.assertEquals(250, auctioneer.getLowestBid(), 0.00001);
+		assertEquals(400, auctioneer.getHighestBid(), 0.00001);
+		assertEquals(250, auctioneer.getLowestBid(), 0.00001);
 
+	}
+
+	@Test
+	public void mustEvaluateAuctionsWithOneBid() {
+		User rob = new User("Rob");
+		Auction auction = new Auction("SmartHome system");
+		auction.proposes(new Bid(rob, 250));
+		Auctioneer auctioneer = new Auctioneer();
+		auctioneer.evaluate(auction);
+
+		assertEquals(250, auctioneer.getHighestBid(), 0.00001);
+		assertEquals(250, auctioneer.getLowestBid(), 0.00001);
+	}
+
+	@Test
+	public void mustFindThreeHighestBids() {
+		User karen = new User("Karen");
+		User louise = new User("Louise");
+		Auction auction = new Auction("Supermagnetic Hoverboard");
+
+		auction.proposes(new Bid(karen, 450));
+		auction.proposes(new Bid(louise, 350));
+		auction.proposes(new Bid(louise, 200));
+		auction.proposes(new Bid(karen, 500));
+
+		Auctioneer auctioneer = new Auctioneer();
+		auctioneer.evaluate(auction);
+
+		List<Bid> highests = auctioneer.get3Highests();
+
+		assertEquals(3, highests.size(), 0.00001);
+		assertEquals(500, highests.get(0).getValue(), 0.00001);
+		assertEquals(450, highests.get(1).getValue(), 0.00001);
+		assertEquals(350, highests.get(2).getValue(), 0.00001);
 	}
 
 }
