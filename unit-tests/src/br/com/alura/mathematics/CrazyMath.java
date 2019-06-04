d5dc80ac23ece85a999c2137b3545e9862224f94
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-04 23:13:29 +0200

    Practicing  tests

---
+++ unit-tests/src/br/com/alura/mathematics/CrazyMath.java
@@ -0,0 +1,14 @@
+package br.com.alura.mathematics;
+
+public class CrazyMath {
+
+	public static int crazyCalculation(int n) {
+		if (n > 30)
+			return n * 4;
+		else if (n > 10)
+			return n * 3;
+		else
+			return n * 2;
+	}
+
+}
---
+++ unit-tests/src/br/com/alura/mathematics/test/TestCrazyMath.java
@@ -0,0 +1,50 @@
+package br.com.alura.mathematics.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import br.com.alura.mathematics.CrazyMath;
+
+public class TestCrazyMath {
+
+	@Test
+	public void biggerThan30() {
+
+		int n = 35;
+		double answer = CrazyMath.crazyCalculation(n);
+		assertEquals(n * 4, answer, 0.0001);
+	}
+	
+	@Test
+	public void equal30() {
+
+		int n = 30;
+		double answer = CrazyMath.crazyCalculation(n);
+		assertEquals(n * 3, answer, 0.0001);
+	}
+	
+	@Test
+	public void between10And30() {
+
+		int n = 20;
+		double answer = CrazyMath.crazyCalculation(n);
+		assertEquals(n * 3, answer, 0.0001);
+	}
+	
+	@Test
+	public void equals10() {
+
+		int n = 10;
+		double answer = CrazyMath.crazyCalculation(n);
+		assertEquals(n * 2, answer, 0.0001);
+	}
+	
+	@Test
+	public void smallerThan10() {
+
+		int n = 1;
+		double answer = CrazyMath.crazyCalculation(n);
+		assertEquals(n * 2, answer, 0.0001);
+	}
+
+}
