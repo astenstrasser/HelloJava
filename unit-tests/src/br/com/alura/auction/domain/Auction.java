e0f2afedbd3fbaafd422b814d0cabe6ebb1d4b7d
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-07 18:54:56 +0200

    Implemented double bid function and improved rules for proposing bids

--- unit-tests/src/br/com/alura/auction/domain/Auction.java
+++ unit-tests/src/br/com/alura/auction/domain/Auction.java
@@ -15,7 +15,28 @@ public class Auction {
 	}
 
 	public void proposes(Bid bid) {
-		bids.add(bid);
+		if (isValid(bid)) {
+			bids.add(bid);
+		}
+	}
+
+	private boolean isValid(Bid bid) {
+		return ((bids.size() == 0) || (getBidAuthor(bids.size() - 1) != bid.getUser()))
+				&& (countBidsFrom(bid.getUser()) < 5);
+	}
+
+	public int countBidsFrom(User user) {
+		int total = 0;
+		for (Bid bid : bids) {
+			if (bid.getUser() == user) {
+				total++;
+			}
+		}
+		return total;
+	}
+
+	private User getBidAuthor(int position) {
+		return bids.get(position).getUser();
 	}
 
 	public String getDescription() {
@@ -24,6 +45,18 @@ public class Auction {
 
 	public List<Bid> getBids() {
 		return Collections.unmodifiableList(bids);
+	}
+
+	public void doubleLastBid(User user) {
+		if (countBidsFrom(user) != 0) {
+			Bid lastBid = null;
+			for (Bid bid : bids) {
+				if (bid.getUser() == user) {
+					lastBid = bid;
+				}
+			}
+			proposes(new Bid(user, lastBid.getValue() * 2));
+		}
 	}
 
 }
