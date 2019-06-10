06701f27381863521f4d6526e5d04073bdc7a869
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-10 15:04:12 +0200

    Reading text file

--- java-io/src/br/com/cursoalura/javaIO/usingJavaIO/ReadingLines.java
+++ java-io/src/br/com/cursoalura/javaIO/usingJavaIO/ReadingLines.java
@@ -1,12 +1,29 @@
 package br.com.cursoalura.javaIO.usingJavaIO;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class ReadingLines {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		
-		
-		
+		InputStream fileInputStream = new FileInputStream("text-file.txt");
+		Reader inputStreamReader = new InputStreamReader(fileInputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+		String line = bufferedReader.readLine();
+
+		while (line != null) {
+			System.out.println(line);
+			line = bufferedReader.readLine();
+		}
+
+		bufferedReader.close();
+
 	}
 
 }
