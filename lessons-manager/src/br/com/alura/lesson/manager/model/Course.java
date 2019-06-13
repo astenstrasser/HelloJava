63b94d3307826946ff85a036cde8ff6ed329245c
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-13 12:17:03 +0200

    Class Course

---
+++ lessons-manager/src/br/com/alura/lesson/manager/model/Course.java
@@ -0,0 +1,35 @@
+package br.com.alura.lesson.manager.model;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Course {
+
+	private String name;
+	private String teacher;
+	private List<Lesson> lessons = new LinkedList<Lesson>();
+
+	public Course(String name, String teacher) {
+		super();
+		this.name = name;
+		this.teacher = teacher;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public List<Lesson> getLessons() {
+		return Collections.unmodifiableList(lessons);
+	}
+	
+	public void add(Lesson lesson) {
+		this.lessons.add(lesson);
+	}
+
+}
