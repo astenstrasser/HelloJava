9f92ac502319462cadd5e0acf0b808883ead3f4f
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-13 12:17:51 +0200

    Tests

---
+++ lessons-manager/tests/br/com/alura/lesson/manager/model/CourseTest.java
@@ -0,0 +1,22 @@
+package br.com.alura.lesson.manager.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CourseTest {
+
+	private Course course;
+
+	@Before
+	public void setUpTests() {
+		this.course = new Course("Java Collections", "Camila Meier");
+		this.course.add(new Lesson("ArrayList 1", 25));
+		this.course.add(new Lesson("LinkedList", 18));
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void mustNotBeAbleToModifyList() {
+		this.course.getLessons().add(new Lesson("Unmodifiable?", 0));
+	}
+	
+}
