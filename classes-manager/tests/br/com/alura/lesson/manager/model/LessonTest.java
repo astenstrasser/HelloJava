3d630e652bbd611abb1d250a4952b8c53dc76785
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-13 11:24:02 +0200

    Renaming

--- classes-manager/tests/br/com/alura/lesson/manager/model/LessonTest.java
+++ classes-manager/tests/br/com/alura/lesson/manager/model/LessonTest.java
@@ -1,45 +1,37 @@
-package br.com.alura.aluraclasses.manager.model;
+package br.com.alura.lesson.manager.model;
 
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class AluraClassTest {
+public class LessonTest {
 
-	private ArrayList<AluraClass> alClasses;
+	private ArrayList<Lesson> lessons;
 
 	@Before
 	public void setUp() {
-		AluraClass c3 = new AluraClass("Java 1", 20);
-		AluraClass c2 = new AluraClass("Objects and Classes", 35);
-		AluraClass c1 = new AluraClass("TDD in Java", 25);
-		this.alClasses = new ArrayList<AluraClass>();
-		this.alClasses.add(c1);
-		this.alClasses.add(c2);
-		this.alClasses.add(c3);
+		Lesson c3 = new Lesson("Java 1", 20);
+		Lesson c2 = new Lesson("Objects and Classes", 35);
+		Lesson c1 = new Lesson("TDD in Java", 25);
+		this.lessons = new ArrayList<Lesson>();
+		this.lessons.add(c1);
+		this.lessons.add(c2);
+		this.lessons.add(c3);
 	}
 
 	@Test
 	public void toStringMethod() {
-		assertEquals("[[TDD in Java, 25], [Objects and Classes, 35], [Java 1, 20]]", alClasses.toString());
+		assertEquals("[[TDD in Java, 25], [Objects and Classes, 35], [Java 1, 20]]", lessons.toString());
 	}
 
 	@Test
 	public void mustOrderByName() {
-		Collections.sort(this.alClasses);
-		assertEquals("[[Java 1, 20], [Objects and Classes, 35], [TDD in Java, 25]]", alClasses.toString());
-	}
-
-	@Test
-	public void mustOrderByDuration() {
-		alClasses.sort(Comparator.comparing(AluraClass::getDuration));
-		assertEquals("[[Java 1, 20], [TDD in Java, 25], [Objects and Classes, 35]]", alClasses.toString());
-
+		Collections.sort(this.lessons);
+		assertEquals("[[Java 1, 20], [Objects and Classes, 35], [TDD in Java, 25]]", lessons.toString());
 	}
 
 }
