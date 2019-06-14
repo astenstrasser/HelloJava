88fc1b236a7c6e1809326fcb5b8cd730ea4891da
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-14 16:39:38 +0200

    Test Students enrollment

--- lessons-manager/tests/br/com/alura/lesson/manager/model/CourseTest.java
+++ lessons-manager/tests/br/com/alura/lesson/manager/model/CourseTest.java
@@ -14,16 +14,29 @@ public class CourseTest {
 		this.course = new Course("Java Collections", "Camila Meier");
 		this.course.add(new Lesson("ArrayList 1", 25));
 		this.course.add(new Lesson("LinkedList", 18));
+		this.course.enroll(new Student("Stud 1", 1111, 1111));
+		this.course.enroll(new Student("Stud 2", 2222, 2222));
 	}
 
 	@Test(expected = UnsupportedOperationException.class)
-	public void mustNotBeAbleToModifyList() {
+	public void mustNotBeAbleToModifyLessonsList() {
 		this.course.getLessons().add(new Lesson("Unmodifiable?", 0));
 	}
 	
 	@Test
 	public void courseDuration() {
 		assertEquals(this.course.getDuration(), 43);
+	}
+	
+	@Test
+	public void enrollment() {
+		assertEquals(this.course.getStudents().size(), 2);
+	}
+	
+	@Test
+	public void isEnrolled() {
+		Student std = new Student("Stud 1", 1111, 1111);
+		assertEquals(true, this.course.isEnrolled(std));
 	}
 	
 }
