cdd3df5ce42f1f3363ecb437471c6528123ea1e0
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-14 16:38:45 +0200

    Added students

--- lessons-manager/src/br/com/alura/lesson/manager/model/Course.java
+++ lessons-manager/src/br/com/alura/lesson/manager/model/Course.java
@@ -1,14 +1,17 @@
 package br.com.alura.lesson.manager.model;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Course {
 
 	private String name;
 	private String teacher;
 	private List<Lesson> lessons = new LinkedList<Lesson>();
+	private Set<Student> students = new HashSet<Student>();
 
 	public Course(String name, String teacher) {
 		super();
@@ -39,10 +42,24 @@ public class Course {
 		}
 		return duration;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "[Course: "+ this.name + " | Teacher: "+this.teacher+"]";
+		return "[Course: " + this.name + " | Teacher: " + this.teacher + "]";
 	}
+
+	public void enroll(Student s) {
+		this.students.add(s);
+	}
+
+	public Set<Student> getStudents() {
+		return Collections.unmodifiableSet(students);
+	}
+	
+	public boolean isEnrolled(Student s) {
+		return this.students.contains(s);
+	}
+	
+	
 
 }
